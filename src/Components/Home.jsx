3ec78bd66b3dfbc5b9b7aef26e39237836ca7a62
Elhassan Ali Abdelhamid <108933298+HassanAliAbdelhamed@users.noreply.github.com>
2025-06-26 23:42:26 +0300

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -64,26 +64,6 @@ export default function Home() {
           {/* <h1 style={{ color: "white", fontSize: "40px" }}>قرآن +</h1> */}
           <Grid
             container
-            spacing={2}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Grid sx={{ color: "white" }} item xs={4}>
-              <AutoCompleteSwarRead Dir="read" Label="قراءة" />
-            </Grid>
-            <Grid sx={{ color: "white" }} item xs={4}>
-              <AutoCompleteSwarRead Dir="tasmee3" Label="تسميع" />
-            </Grid>
-            <Grid sx={{ color: "white" }} item xs={4}>
-              <AutoCompleteReader />
-            </Grid>
-          </Grid>
-
-          <Grid
-            container
             sx={{ width: "100%", position: "relative", marginTop: "20px" }}
           >
             {swar.map((qara) => (
