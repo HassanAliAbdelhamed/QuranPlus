974c727f47699bb44ca32d6a48d0fcdb580daf07
Elhassan Ali <zxxswe12@gmail.com>
2026-02-18 02:20:56 +0200

    Solve Some Errors For Publishing

--- src/Components/Sound.jsx
+++ src/Components/Sound.jsx
@@ -11,7 +11,7 @@ import SoundImage from '../Imgs/SoundImage.png'
 import QareImage from '../Imgs/QareImage.png'
 function Player() {
   const [anchorEl, setAnchorEl] = useState(null);
-
+  anchorEl ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto'; // Disable scrolling when menu is open
   // Open and close the menu
   const handleMenuClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -22,20 +22,22 @@ function Player() {
   let swar= AllSwar;
   let par = useParams();
   let navigate = useNavigate();
-  const [swara, setSwara] = useState(par.swara);
-  const [id, setId] = useState(par.id);
+  let [swara, setSwara] = useState(par.swara);
+  console.log(swara);
+  const [id] = useState(par.id);
   const [swara1, setSwara1] = useState(swar[parseInt(par.swara) - 1]);
   const [swaraTitle, setSwaraTitle] = useState(swara1?.title);
   const [swaraId, setSwaraId] = useState(swara1?.id);
   const [urlAudio, setUrlAudio] = useState(quraa[id]?.Audiosource + par.swara + ".mp3");
   
   useEffect(() => {
+    setSwara(par.swara);
     const newSwara1 = swar[parseInt(par.swara) - 1];
     setSwara1(newSwara1);
     setSwaraTitle(newSwara1?.title);
     setSwaraId(newSwara1?.id);
     setUrlAudio(`${quraa[id]?.Audiosource}${par.swara}.mp3`);
-  }, [id, par.swara, swar]);
+  }, [id, par.swara, par.id,swar]);
  
 
   return (
